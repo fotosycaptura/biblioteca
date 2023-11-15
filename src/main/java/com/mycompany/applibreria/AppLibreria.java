15d7eb4ac6b6cc13d5c758c30e92b0d0c0710d49
fotosycaptura <charsxavier@gmail.com>
2023-11-15 20:02:42 -0300

    Se agregan comentarios de créditos en cada archivo

--- src/main/java/com/mycompany/applibreria/AppLibreria.java
+++ src/main/java/com/mycompany/applibreria/AppLibreria.java
@@ -11,6 +11,13 @@ import java.util.Arrays;
 /**
  *
  * @author Tom
+ * Clase entregada para trabajar en ella por el profesor
+ * Modificado por integrantes del grupo 2:
+ * Xavier Fuentes
+ * Carlos Anch
+ * Felipe González
+ * Sebastian Lantadilla
+ * Ivan Rojas
  */
 public class AppLibreria {
 
--- src/main/java/com/mycompany/applibreria/Estudiante.java
+++ src/main/java/com/mycompany/applibreria/Estudiante.java
@@ -7,6 +7,13 @@ package com.mycompany.applibreria;
 /**
  *
  * @author Tom
+ * Clase entregada para trabajar en ella por el profesor
+ * Modificado por integrantes del grupo 2:
+ * Xavier Fuentes
+ * Carlos Anch
+ * Felipe González
+ * Sebastian Lantadilla
+ * Ivan Rojas
  */
 public class Estudiante extends Usuario {
     private String carrera;
