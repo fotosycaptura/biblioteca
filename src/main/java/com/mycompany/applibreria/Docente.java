9aca38842a31a8752f9245b08524ead273cb2e55
fotosycaptura <charsxavier@gmail.com>
2023-11-14 21:54:20 -0300

    Se agregan atributos y métodos iniciales

--- src/main/java/com/mycompany/applibreria/Docente.java
+++ src/main/java/com/mycompany/applibreria/Docente.java
@@ -9,7 +9,50 @@ package com.mycompany.applibreria;
  * @author Tom
  */
 public class Docente extends Usuario {
+    private String gradoAcademico;
+    private String profesion;
+    
     public Docente(String RUN) {
         super(RUN);
+    }
+    
+    /**
+     * @return the gradoAcademico
+     */
+    public String getGradoAcademico() {
+        return gradoAcademico;
+    }
+
+    /**
+     * @param gradoAcademico the gradoAcademico to set
+     */
+    public void setGradoAcademico(String gradoAcademico) {
+        this.gradoAcademico = gradoAcademico;
+    }
+
+    /**
+     * @return the profesion
+     */
+    public String getProfesion() {
+        return profesion;
+    }
+
+    /**
+     * @param profesion the profesion to set
+     */
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+    
+    /**
+     * Sobreescribe el método toString para desplegar valores personalizados.
+     */
+    @Override
+    public String toString() {
+        return "Run: " + getRUN() + "\t" +
+                "Nombre: " + getNombre() + "\t" +
+                "Profesión: " + getProfesion() + "\t" +
+                "Grado Académico: " + getGradoAcademico() + "\t" +
+                "Estado: " + getVigente();
     }
 }
