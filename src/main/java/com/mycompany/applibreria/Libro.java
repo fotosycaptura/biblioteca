08cdbd4658aab0e3b3e83679e59ebf9c4827f705
fotosycaptura <charsxavier@gmail.com>
2023-11-14 21:42:30 -0300

    Limpieza

--- src/main/java/com/mycompany/applibreria/Libro.java
+++ src/main/java/com/mycompany/applibreria/Libro.java
@@ -10,6 +10,10 @@ package com.mycompany.applibreria;
  */
 public class Libro {
     private int ISBN;
+    private String titulo;
+    private int cantidadDisponibleBiblioteca;
+    private int cantidadDisponiblePrestamo;
+    private String imagen;
     
     // DEBE COMPLETAR ESTE CONSTRUCTOR
     public Libro(int ISBN) {
@@ -28,5 +32,61 @@ public class Libro {
      */
     public void setISBN(int ISBN) {
         this.ISBN = ISBN;
+    }
+    
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the cantidadDisponibleBiblioteca
+     */
+    public int getCantidadDisponibleBiblioteca() {
+        return cantidadDisponibleBiblioteca;
+    }
+
+    /**
+     * @param cantidadDisponibleBiblioteca the cantidadDisponibleBiblioteca to set
+     */
+    public void setCantidadDisponibleBiblioteca(int cantidadDisponibleBiblioteca) {
+        this.cantidadDisponibleBiblioteca = cantidadDisponibleBiblioteca;
+    }
+
+    /**
+     * @return the cantidadDisponiblePrestamo
+     */
+    public int getCantidadDisponiblePrestamo() {
+        return cantidadDisponiblePrestamo;
+    }
+
+    /**
+     * @param cantidadDisponiblePrestamo the cantidadDisponiblePrestamo to set
+     */
+    public void setCantidadDisponiblePrestamo(int cantidadDisponiblePrestamo) {
+        this.cantidadDisponiblePrestamo = cantidadDisponiblePrestamo;
+    }
+
+    /**
+     * @return the imagen
+     */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * @param imagen the imagen to set
+     */
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
     }
 }
