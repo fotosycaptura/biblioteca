86b95c017a3ba870a9cb5d770c69a179c7e18aa0
fotosycaptura <charsxavier@gmail.com>
2023-11-14 21:54:52 -0300

    Se agregan atributos de estudiante

--- src/main/java/com/mycompany/applibreria/Estudiante.java
+++ src/main/java/com/mycompany/applibreria/Estudiante.java
@@ -9,8 +9,34 @@ package com.mycompany.applibreria;
  * @author Tom
  */
 public class Estudiante extends Usuario {
+    private String carrera;
     // GENERAMOS DATOS DE USUARIOS BASE
     public Estudiante(String RUN) {
         super(RUN);
+    }
+    
+    /**
+     * @return the carrera
+     */
+    public String getCarrera() {
+        return carrera;
+    }
+
+    /**
+     * @param carrera the carrera to set
+     */
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+    
+    /**
+     * Sobreescribe el método toString para desplegar valores personalizados.
+     */
+    @Override
+    public String toString() {
+        return "Run: " + getRUN() + "\t" +
+                "Nombre: " + getNombre() + "\t" +
+                "Carrera: " + getCarrera() + "\t" +
+                "Estado: " + getVigente();
     }
 }
