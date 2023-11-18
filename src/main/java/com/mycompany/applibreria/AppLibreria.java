79c626ac8a221482e7169ebd54e81360633a7f72
fotosycaptura <40148698+fotosycaptura@users.noreply.github.com>
2023-11-18 15:05:48 -0300

    Update AppLibreria.java
    Se ordena un poco más el código

--- src/main/java/com/mycompany/applibreria/AppLibreria.java
+++ src/main/java/com/mycompany/applibreria/AppLibreria.java
@@ -91,13 +91,19 @@ public class AppLibreria {
         
         // UN ARREGLO DE Préstamos
         ArrayList<Prestamo> prestamos = new ArrayList<Prestamo>();
-        // GENERAMOS UN PRÉSTAMO
+        
         System.out.println("Visualizando libro a prestar: " + Libro.verLibro(1, libros));
+        
+        // GENERAMOS UN PRÉSTAMO
         Prestamo prestamo = Prestamo.ingresarPrestamo(1, "11111111-1", libros, usuarios);
+        
+        //Se visualiza si se llevó a cabo el descuento de disponibles para préstamo por haberlo prestado.
         System.out.println("Visualizando libro a ver si se prestó o no: " + Libro.verLibro(1, libros));
         
         // AGREGAMOS EL PRÉTAMO AL ARREGLO DE PRÉSTAMOS
         prestamos.add(prestamo);
+        
+        
         // IMPRIMIMOS EL ESTADO ACTUAL DEL PRÉSTAMO
         System.out.println(prestamo.toString());
         // GENERAMOS UNA DEVOLUCION
